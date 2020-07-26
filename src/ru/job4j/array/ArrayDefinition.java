37a9db11f34f0cfc847e36141f8ffae051f92422
LeFor888 <Ytt,fwfxticrbqghjuth>
2020-07-26 23:04:44 +0300

    6.0.2. Заполнение массива. [#274362]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,15 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
     short[] ages = new short[10];
-    String[] surnames = new String[100500];
+    String[] names = new String[4];
     float[] prices = new float[40];
+    names[0] = "Stefan Zweig";
+    names[1] = "Andrzej Sapkowski";
+    names[2] = "Jaroslav Hašek";
+    names[3] = "Jan Hus";
+    System.out.println(names[0]);
+    System.out.println(names[1]);
+    System.out.println(names[2]);
+    System.out.println(names[3]);
     }
 }

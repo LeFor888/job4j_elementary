21bb8e5adde29517cb6fd2ef45b9568d389c840d
LeFor888 <Ytt,fwfxticrbqghjuth>
2020-07-31 19:03:14 +0300

    6.3. Массив заполнен true или false [#274351]

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -4,7 +4,7 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
              for (int index = 0; index < data.length - 1; index++) {
-                 if (!data[index]) {
+                 if (data[index] != data[index + 1]) {
                      return false;
                  }
              }

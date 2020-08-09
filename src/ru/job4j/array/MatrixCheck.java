03ac051c3a20b1c73003207bb796f233c63e5bc1
Lefor888 <vitalfonpuhen@gmail.com>
2020-08-09 19:37:23 +0300

    6.7.4. Выигрышные комбинации в сокобан [#274352]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,17 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int index = 0; index < board.length; index++) {
+            if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
             for (int column = 0; column < board.length; column++) {

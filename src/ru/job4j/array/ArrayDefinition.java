5271faed1dde224c4f658f456aa1afad264526d0
Lefor888 <vitalfonpuhen@gmail.com>
2021-05-28 01:33:26 +0300

    6.0.1.1. Размер массива [#333582]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -11,13 +11,13 @@ public class ArrayDefinition {
             System.out.println(nums[i]);
         }
 
-    short[] ages = new short[10];
-    String[] names = new String[4];
-    float[] prices = new float[40];
-    names[0] = "Stefan Zweig";
-    names[1] = "Andrzej Sapkowski";
-    names[2] = "Jaroslav Hašek";
-    names[3] = "Jan Hus";
+        short[] ages = new short[10];
+        String[] names = new String[4];
+        float[] prices = new float[40];
+        names[0] = "Stefan Zweig";
+        names[1] = "Andrzej Sapkowski";
+        names[2] = "Jaroslav Hašek";
+        names[3] = "Jan Hus";
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
